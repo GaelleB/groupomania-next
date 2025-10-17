e52dc04c60f103cc2b8d2bdb2cc490bd8b22b4c7
Gaelle Boucher <gaelleboucher.dev@gmail.com>
2025-10-17 21:40:29 +0200

    Backend: Middlewares d'authentification et upload
    - Middleware JWT pour authentification
    - Middleware Multer pour upload d'images
    - Sécurisation des routes

---
+++ backend/middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'SECRET_TOKEN');
+        const userId = decodedToken.userId;
+        req.tokenUserId = userId;
+        if(req.body.UserId && req.body.UserId !== userId) {
+            throw 'err middleware auth ID utilisateur incorrect !';
+        } else {
+            next();
+        }
+    } catch(error) {
+        res.status(401).json({error});
+    }
+}
