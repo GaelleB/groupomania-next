af82a341c461541750d772e57bc0ffe57e9c52ba
Gaelle Boucher <gaelleboucher.dev@gmail.com>
2025-10-18 11:53:15 +0200

    Config: Qualité de code et tests automatisés
    - Configuration ESLint avec règles strictes
    - Configuration Prettier pour formatage cohérent
    - Configuration Jest pour tests unitaires
    - Scripts npm: test, lint, format, dev, start
    - Test coverage activé
    - Indentation 4 espaces, guillemets simples, semicolons

---
+++ backend/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    testEnvironment: 'node',
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        '**/*.js',
+        '!**/node_modules/**',
+        '!**/coverage/**',
+        '!**/logs/**',
+        '!**/tests/**',
+        '!jest.config.js'
+    ],
+    testMatch: [
+        '**/tests/**/*.test.js'
+    ],
+    verbose: true,
+    forceExit: true,
+    clearMocks: true,
+    resetMocks: true,
+    restoreMocks: true
+};
