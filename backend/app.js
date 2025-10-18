fd771ddc1c7d3636f5bb5a43394d55cba9d426ce
Gaelle Boucher <gaelleboucher.dev@gmail.com>
2025-10-18 10:52:51 +0200

    Security: Configuration CORS sécurisée avec whitelist
    - Remplacement de Access-Control-Allow-Origin: * par une whitelist
    - Configuration des origines autorisées (localhost + FRONTEND_URL env)
    - Support de Access-Control-Allow-Credentials
    - Ajout de dotenv.config() pour charger les variables d'environnement

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,11 @@ authSequelize();
 
 // Sécurité
 const dotenv = require ('dotenv');
+dotenv.config();
+const helmet = require('helmet');
+
+// Protection avec Helmet (headers HTTP sécurisés)
+app.use(helmet());
 
 // Importation des routes
 const userRoutes = require("./routes/user");
@@ -16,9 +21,19 @@ const commentRoutes = require("./routes/comment");
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-// CORS
+// CORS - Configuration sécurisée avec whitelist
+const allowedOrigins = [
+    process.env.FRONTEND_URL || 'http://localhost:3001',
+    'http://localhost:3001',
+    'http://localhost:3000'
+];
+
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+    }
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
